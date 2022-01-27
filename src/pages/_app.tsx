375f4eb2aa4aa078a2371096f77abaa29570cc51
jcarvajal1987 <jorgecarvajal1999@gmail.com>
2022-01-27 02:15:10 -0300

    add home and Dashboard

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,17 @@
 import React from "react"
 
 import { AppProps } from "next/app"
+
 import "../styles/tailwind.scss"
+import { NavBar } from "@components/NavBar"
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
